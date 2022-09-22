f6e330faae6db3265e08350d1379790617955030
Paloma Cecilia Soares da Costa <palomacissa20096@gmail.com>
2022-09-22 08:24:10 -0300

    Aula sobre variáveis e constantes - Python

---
+++ Python - primeiros passos/variaveis_constantes.py
@@ -0,0 +1,16 @@
+nome = "Paloma"
+idade = "27"
+
+print (nome,idade)
+
+nome = "Teste"
+
+print (nome, idade)
+
+nome, idade = "Teste", "30"
+
+limite_saque_diario = 1000
+
+STATES = ["SP", "RJ", "MG"]
+
+print (STATES)
